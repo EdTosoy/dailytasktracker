d35c5020e37a834c2db7377af622f4c8d57aeb3e
edtosoy <edberto.tosoy@gmail.com>
2025-05-10 12:21:21 +0800

    publish Develop branch

--- src/pages/AdminDashboard/AdminDashboard.tsx
+++ src/pages/AdminDashboard/AdminDashboard.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 type Props = {};
 
 export default function AdminDashboard({}: Props) {
-  return <div>THIS IS THE ADMIN DASHBOARD</div>;
+  return <div>THIS IS THE ADMIN DASHBOARD!</div>;
 }
